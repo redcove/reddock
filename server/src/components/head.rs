c3f133ef679521e7ada4f13b530f65d84ff4deec
redcove <redcoveport@pm.me>
2022-02-24 23:16:21 +0800

    Update head & custom css

--- server/src/components/head.rs
+++ server/src/components/head.rs
@@ -3,6 +3,9 @@ use dioxus::prelude::*;
 pub fn head(cx: Scope) -> Element {
     cx.render(rsx!(
         head {
+            meta { name: "viewport", content: "width=device-width, initial-scale=1" }
+            meta { charset: "utf-8" }
+            title { "Red Dock" }
             style { [include_str!("../../static/pico.min.css")] }
         }
     ))
