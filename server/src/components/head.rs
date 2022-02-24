ec9b9fd932f0e5653ffd167535c33892f1391066
redcove <redcoveport@pm.me>
2022-02-24 22:55:09 +0800

    Replace spectre with pico

--- server/src/components/head.rs
+++ server/src/components/head.rs
@@ -3,7 +3,7 @@ use dioxus::prelude::*;
 pub fn head(cx: Scope) -> Element {
     cx.render(rsx!(
         head {
-            style { [include_str!("../../static/spectre.min.css")] }
+            style { [include_str!("../../static/pico.min.css")] }
         }
     ))
 }
